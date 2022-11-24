0468403d408abd9aebdc1f15283fce20ee6c859e
Luke Wiwatowski <luke.wiwatowski@wsp.com>
2022-11-24 01:09:42 +0000

    Add tests for checking the uuid generation

--- tests/test_django_byo_react.py
+++ tests/test_django_byo_react.py
@@ -1,5 +1,8 @@
+from unittest.mock import patch
 from django.template import Context, Template
 from django.test import TestCase
+
+from django_byo_react.templatetags.byo_react import uuid
 
 
 class ByoReactTestCase(TestCase):
@@ -9,6 +12,16 @@ class ByoReactTestCase(TestCase):
         context = context or {}
         context = Context(context)
         return Template(string).render(context)
+
+    @patch.object(uuid, "uuid4", side_effect=["one", "two"])
+    def test_json_script_renders(self, mock_uuid):
+        rendered = self.render_template(
+            """{% load byo_react %}
+            {% byo_react %}"""
+        )
+        self.assertInHTML(
+            '<script id="two" type="application/json">{}</script>', rendered
+        )
 
     def test_json_script_passes_true_false(self):
         rendered = self.render_template(
@@ -34,10 +47,22 @@ class ByoReactTestCase(TestCase):
         )
         self.assertInHTML('{"testDict": {"test": "test"}}', rendered)
 
-    def test_classname_renders(self):
+    @patch.object(uuid, "uuid4", side_effect=["one", "two"])
+    def test_classname_renders(self, mock_uuid):
         rendered = self.render_template(
             """{% load byo_react %}
-            {% byo_react id=test_id className='w-100 h-100' %}""",
-            context={"test_id": self.test_id},
+            {% byo_react className='w-100 h-100' %}""",
         )
-        self.assertIn('class="w-100 h-100"', rendered)
+        self.assertInHTML(
+            '<div id="one" data-script-id="two" class="w-100 h-100"></div>', rendered
+        )
+
+    @patch.object(uuid, "uuid4", side_effect=["one", "two"])
+    def test_uuid_id(self, mock_uuid):
+        rendered = self.render_template(
+            """{% load byo_react %}
+            {% byo_react %}"""
+        )
+        self.assertInHTML(
+            '<div id="one" data-script-id="two"></div>', rendered, msg_prefix=rendered
+        )
